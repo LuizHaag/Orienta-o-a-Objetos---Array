0b8d1c0fae7ce22f23a294d0f65eefc1ae49ce0c
LuizHaag <93357649+LuizHaag@users.noreply.github.com>
2021-12-02 18:51:38 -0300

    Add files via upload

---
+++ exercicio1_oo.cpp
@@ -0,0 +1,44 @@
+/*
+Inplemente um sistema onde o usuario entre com sinco valores
+e mostre quantos sao repetidos quais suas posicao
+*/
+
+	#include <iostream>
+	using namespace std;
+	
+		class posicao{	
+		int numero;
+		int resultado;
+		public:
+			posicao2(int valor);
+		};
+
+			int posicao::posicao2(int i){
+			//numero  >> i == 3 ;
+			cout  << "Digite os Valore:: " << endl ;
+			cin >> numero ;
+				}	
+			int main(){
+			int resultado, i,j;
+			int valor[5] ;
+			posicao Minhaposicao2;	
+			for (i = 0;i<5;i++){
+			cout <<"digite os valores <<" << endl;
+			cin  >> valor[i];
+			}
+			
+			for (i =0;i<5;i++){
+				for (j =i;j<5;j++){
+				if(valor[i]==valor[j]){
+       		
+      }
+					
+			 }			
+				
+			}
+			
+					
+		return 0;
+	}
+		
+		
